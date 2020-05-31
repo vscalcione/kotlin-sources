32ff758ef9fb72fb1c1ec20de5e0d95768bd1e3a
Vincenzo Scalcione <vincenzo99.scalcione@gmail.com>
2020-05-31 15:50:23 +0200

    Add HelloWorld.kt

---
+++ src/it/kotlin/course/HelloWorld.kt
@@ -0,0 +1,13 @@
+package it.kotlin.course
+
+class HelloWorld {
+
+    fun printMessage(message: String) {
+        print(message);
+    }
+}
+
+fun main() {
+    var HelloWorld = HelloWorld();
+    HelloWorld.printMessage("I'm a Kotlin file!!! ");
+}
